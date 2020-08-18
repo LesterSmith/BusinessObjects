f9c692bd889e7e6e062f0b41344f9ae1b804171a
LesterSmith <46542486+LesterSmith@users.noreply.github.com>
2020-08-18 14:38:13 -0400

    new User class

---
+++ User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BusinessObjects
+{
+    public class User
+    {
+        #region public members
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string PermissionLevel { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        #endregion // public members
+
+    }
+}
